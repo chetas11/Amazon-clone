de656c5358d746908c26dfd51914225318466592
chetas11 <cbirmole@gmail.com>
2021-02-22 17:28:12 +0530

    header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD9N92ZG_i2HGO39fhLPgUS9bxSYRQDYO8",
+  authDomain: "clone-16268.firebaseapp.com",
+  projectId: "clone-16268",
+  storageBucket: "clone-16268.appspot.com",
+  messagingSenderId: "1028395352263",
+  appId: "1:1028395352263:web:45ac7d7cc198125cae7ee2",
+  measurementId: "G-YWBTC7YMKP",
+};
